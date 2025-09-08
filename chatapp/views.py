6d5765b38f9f8723201ed4e1be06e999d01ac975
ibtisam14 <ibtsamulhaq14@gmail.com>
2025-09-08 10:31:16 +0500

    fix issue

--- chatapp/views.py
+++ chatapp/views.py
@@ -1,7 +1,11 @@
-# chatapp/views.py
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from chat.models import Chat
 
+@login_required
 def home(request):
-    chats = Chat.objects.all()  # or filter by request.user
-    return render(request, "home.html", {"chats": chats})
+    chats = Chat.objects.filter(participants=request.user)
+
+    chats_with_status = [{"chat": chat} for chat in chats]
+
+    return render(request, "home.html", {"chats": chats_with_status})
